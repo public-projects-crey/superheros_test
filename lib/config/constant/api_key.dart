514be352695442117ac5a596d643d91c261a2fa5
carlrey9 <crey388@unab.edu.co>
2023-07-06 07:37:43 +0900

    keys public

---
+++ lib/config/constant/api_key.dart
@@ -0,0 +1,3 @@
+final PUBLICKEY = "fea4ddbf370376865724c2b03db5ffef";
+final PRIVATEKEY = "dcca949708ec8a3a3b97cf51cacf65cd98a314c4";
+final ENDPOINT = "https://gateway.marvel.com:443/v1/public/characters";
